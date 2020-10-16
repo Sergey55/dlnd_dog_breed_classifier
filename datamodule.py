4598a5558277eaaa9284e6638494ecb8afe60230
Sergey Andreev <sergey666andreev@gmail.com>
2020-10-16 11:23:44 +0600

    feat: Add double underscore to create* methods
    In order to highlight that create methods for internal use rename then
    and prepend `__`

--- datamodule.py
+++ datamodule.py
@@ -13,7 +13,7 @@ class DogsDataModule(pl.LightningDataModule):
         self.num_workers = num_workers
 
     def train_dataloader(self):
-        train_dataset = ImageFolder(root=self.data_paths['train'], transform=self.create_train_transformation())
+        train_dataset = ImageFolder(root=self.data_paths['train'], transform=self.__create_train_transformation())
 
         return DataLoader(
             train_dataset,
@@ -22,7 +22,7 @@ class DogsDataModule(pl.LightningDataModule):
             num_workers = self.num_workers)
 
     def val_dataloader(self):
-        validation_dataset = ImageFolder(root=self.data_paths['val'], transform=self.create_validation_transformation())
+        validation_dataset = ImageFolder(root=self.data_paths['val'], transform=self.__create_validation_transformation())
 
         return DataLoader(
             validation_dataset,
@@ -30,14 +30,14 @@ class DogsDataModule(pl.LightningDataModule):
             num_workers = self.num_workers)
 
     def test_dataloader(self):
-        test_dataset = ImageFolder(root=self.data_paths['test'], transform=self.create_test_transformation())
+        test_dataset = ImageFolder(root=self.data_paths['test'], transform=self.__create_test_transformation())
 
         return  DataLoader(
             test_dataset,
             batch_size = self.batch_size,
             num_workers = self.num_workers)
 
-    def create_normalization_transformation(self):
+    def __create_normalization_transformation(self):
         """Create normalization transformation common for all datasets
 
         Returns:
@@ -46,7 +46,7 @@ class DogsDataModule(pl.LightningDataModule):
         return transforms.Normalize((0.485, 0.456, 0.406),
                                     (0.229, 0.224, 0.225))
 
-    def create_train_transformation(self) -> torchvision.transforms.Compose:
+    def __create_train_transformation(self) -> torchvision.transforms.Compose:
         """Create appropriate transformation for train dataset
 
         Returns:
@@ -56,20 +56,10 @@ class DogsDataModule(pl.LightningDataModule):
             transforms.Resize((256, 256)),
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            self.create_normalization_transformation()])
+            transforms.ToTensor()
+        ])
 
-    def create_validation_transformation(self) -> torchvision.transforms.Compose:
-        """Create appropriate transformation for validation dataset
-
-        Returns:
-            Instance of torchvision.transforms.Compose class"""
-        return transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            self.create_normalization_transformation()])
-
-    def create_test_transformation(self) -> torchvision.transforms.Compose:
+    def __create_validation_transformation(self) -> torchvision.transforms.Compose:
         """Create appropriate transformation for validation dataset
 
         Returns:
@@ -77,5 +67,17 @@ class DogsDataModule(pl.LightningDataModule):
 
         return transforms.Compose([
             transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            self.create_normalization_transformation()])
+            transforms.ToTensor()
+        ])
+
+    def __create_test_transformation(self) -> torchvision.transforms.Compose:
+        """Create appropriate transformation for test dataset
+
+        Returns:
+            Instance of torchvision.transforms.Compose class"""
+
+        return transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
+            ]
+        )

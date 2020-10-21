36d46930d79f71a201169b8fc6bb1b2bb14bd1ee
Sergey Andreev <sergey666andreev@gmail.com>
2020-10-21 15:45:21 +0600

    feat: Add `evaluatept.py` script
    Add script for obtaining prediction by image.

--- train.py
+++ train.py
@@ -1,38 +1,20 @@
-import os
-
 from pytorch_lightning import Trainer
 from argparse import ArgumentParser
 
-from datamodule import DogsDataModule
-
-from net import DogsBreedClassifier
-from net_transfer_densenet121 import DogsBreedClassifierDenseNet
-from net_transfer_EfficientNet import DogsBreedClassifierEfficientNet
+from util import create_data_module, create_model, create_config
 
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def main(args):
-    
-    if args.mode == 'scratch':
-        print('Using `Scratch` model')
-        model = DogsBreedClassifier()
-    elif args.mode == 'densenet':
-        print('Using `DenseNet121` network')
-        model = DogsBreedClassifierDenseNet()
-    else:
-        print('Using `EfficientNet` network')
-        model = DogsBreedClassifierEfficientNet()
-
-    paths = {
-        'train': './dog_images/train/',
-        'test': './dog_images/test/',
-        'val': './dog_images/valid/'
-    }
-
-    dm = DogsDataModule(paths)
+    config = create_config()
+    model = create_model(args.mode)
+    dm = create_data_module(config)
 
     print(f'Training for {args.max_epochs} epochs')
+    
+    # Output training parameters
+    config.display()
 
     trainer = Trainer.from_argparse_args(args)
     trainer.fit(model, dm)
